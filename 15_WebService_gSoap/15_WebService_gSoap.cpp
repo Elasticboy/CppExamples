b71028784710971011840d8614c07e8bf06609ab
Cyril Leroux <cyril.leroux@cgi.com>
2013-02-28 15:23:57 +0100

    Added 15_WebService_gSoap project to CppExamples solution

---
+++ 15_WebService_gSoap/15_WebService_gSoap.cpp
@@ -0,0 +1,23 @@
+// 15_WebService_gSoap.cpp : Defines the entry point for the console application.
+//
+#include "gsoap\soapH.h" // obtain the generated stub  
+#include "gsoap\GeoIPServiceSoap.nsmap" // obtain the generated XML namespace mapping table for the GeoIPServiceSoap service  
+
+int main(int argc, char** argv)
+{
+	//soap* mySoap = soap_new();
+	//const std::string ip = "217.154.243.205";
+	//_ns1__GetGeoIP geoIp;
+	//geoIp.IPAddress->assign(ip);
+	//_ns1__GetGeoIPResponse response;
+	//if ( soap_call___ns1__GetGeoIP(mySoap, nullptr, nullptr, &geoIp, &response) == SOAP_OK) {
+	//	std::cout << "Current Geo is " << response.GetGeoIPResult->CountryName << std::endl;
+
+	//} else { // an error occurred  
+ //     soap_print_fault(mySoap, stderr); // display the SOAP fault on the stderr stream  
+	//}
+	
+	system("PAUSE");
+	return 0;
+}
+
