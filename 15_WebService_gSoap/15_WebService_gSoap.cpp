d3c07621311bcb9fa73b1974fb26c25f202f3628
Cyril Leroux <cyril.leroux@cgi.com>
2013-03-01 15:23:33 +0100

    Tries to make gSoap work.

--- 15_WebService_gSoap/15_WebService_gSoap.cpp
+++ 15_WebService_gSoap/15_WebService_gSoap.cpp
@@ -1,22 +1,33 @@
 // 15_WebService_gSoap.cpp : Defines the entry point for the console application.
 //
-#include "gsoap\soapH.h" // obtain the generated stub  
+#include "gsoap\stdsoap2.h"
+#include "gsoap\soapStub.h" // obtain the generated stub  
 #include "gsoap\GeoIPServiceSoap.nsmap" // obtain the generated XML namespace mapping table for the GeoIPServiceSoap service  
+
+struct Namespace *v_namespaces;
 
 int main(int argc, char** argv)
 {
-	//soap* mySoap = soap_new();
-	//const std::string ip = "217.154.243.205";
-	//_ns1__GetGeoIP geoIp;
-	//geoIp.IPAddress->assign(ip);
-	//_ns1__GetGeoIPResponse response;
-	//if ( soap_call___ns1__GetGeoIP(mySoap, nullptr, nullptr, &geoIp, &response) == SOAP_OK) {
-	//	std::cout << "Current Geo is " << response.GetGeoIPResult->CountryName << std::endl;
+	struct soap soap;
+	soap_init(&soap);
+	/*
+	_ns1__GetGeoIP geoIp;
+	geoIp.IPAddress->assign("217.154.243.205");
+	_ns1__GetGeoIPResponse response;
 
-	//} else { // an error occurred  
- //     soap_print_fault(mySoap, stderr); // display the SOAP fault on the stderr stream  
-	//}
-	
+	//if (soap_call___ns1__GetGeoIP(&soap, nullptr, nullptr, )
+	if (soap_call___ns1__GetGeoIP(&soap, "http://www.webservicex.net/geoipservice.asmx?WSDL", "", &geoIp, &response) == SOAP_OK) {
+		std::cout << "Country : " << response.GetGeoIPResult->CountryName << std::endl; 
+
+	} else {
+		soap_print_fault(&soap, stderr); // display the SOAP fault message on the stderr stream 
+	}
+
+	soap_destroy(&soap); // delete deserialized class instances (for C++) 
+	soap_end(&soap); // remove deserialized data and clean up 
+	soap_done(&soap); // detach the gSOAP context
+	*/
+
 	system("PAUSE");
 	return 0;
 }
