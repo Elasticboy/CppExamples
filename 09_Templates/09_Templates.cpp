8f5dbaf4c638e7e510896c0c33e00a6189fd9ac1
Cyril Leroux <cyril0leroux@gmail.com>
2013-03-10 19:29:57 +0100

    Updated template project to test getString function

--- 09_Templates/09_Templates.cpp
+++ 09_Templates/09_Templates.cpp
@@ -38,13 +38,31 @@ template <typename T> void testArithmeticOrLiteral(const T& t)
 	}
 }
 
+/////////////////////////////////
+
+template<typename T> std::string getString(const std::string& str, const T& arg, std::true_type) {
+	return str + to_string(arg);
+}
+
+template<typename T> std::string getString(const std::string& str, const T& arg, std::false_type) {
+	return str + arg;
+}
+
+template<typename T> std::string getString(const std::string& str, const T& arg)
+{
+	return getString(str, arg, is_arithmetic<T>::type());
+}
+
+///////////////////////////////////
+
 int main()
 {
 	// Test templates
+	cout << "Test templates" << endl;
 	testIntegral(122);
 	testIntegral(1.22f);
 	testIntegral("e");
-    cout << endl; 
+	cout << endl; 
 
 	const int const_int(10);
 	int test_int(8);
@@ -58,55 +76,66 @@ int main()
 	testArithmeticOrLiteral("bob");
 	testArithmeticOrLiteral(L"bob");
 	testArithmeticOrLiteral(string("bob"));
-    cout << endl; 
+
+	cout << endl; 
 
 	// Test type_traits
+	cout << "Test type_traits" << endl;
 	cout << boolalpha;
-    cout << is_arithmetic<int>::value << endl;
-    cout << is_arithmetic<int&>::value << endl;
-    cout << is_arithmetic<int*>::value << endl;
-    cout << is_arithmetic<float>::value << endl;
-    cout << is_arithmetic<float&>::value << endl;
-    cout << is_arithmetic<float*>::value << endl;
-	
-    cout << endl; 
-	
+	cout << "is_arithmetic<int> "		<< is_arithmetic<int>::value << endl;
+	cout << "is_arithmetic<int&> "		<< is_arithmetic<int&>::value << endl;
+	cout << "is_arithmetic<int*> "		<< is_arithmetic<int*>::value << endl;
+	cout << "is_arithmetic<float> "		<< is_arithmetic<float>::value << endl;
+	cout << "is_arithmetic<float&> "	<< is_arithmetic<float&>::value << endl;
+	cout << "is_arithmetic<float*> "	<< is_arithmetic<float*>::value << endl;
+
+	cout << endl; 
+
+	// Test Variadic templates
+	cout << "Test Variadic templates" << endl;
 	safe_print("bobob", 123, 122.354, string("qdsf"));
-	//getString("bobob", 123.123);
-	//getString("bobob", "HELLO");
-	//getString("bobob", string("STRING"));
+	
+	cout << endl; 
+
+	// Test getString
+	cout << "Test getString" << endl;
+	cout << getString("int    ", 1234) << endl;
+	cout << getString("double ", 12.34) << endl;
+	cout << getString("float  ", 12.34f) << endl;
+	cout << getString("char*  ", "HELLO") << endl;
+	cout << getString("string ", string("STRING")) << endl;
 
 	system("PAUSE");
 	return 0; 
 }
 
-	/** is_arithmetic */
-	template<typename T> typename std::enable_if<std::is_integral<T>::value, long>::type
+/** is_arithmetic */
+template<typename T> typename std::enable_if<std::is_integral<T>::value, long>::type
 	normalizeArg(const T& arg) {
 		cout << "--is_arithmetic" << endl;
 		return arg;
-	}
-	
-	/** is_arithmetic */
-	template<typename T> typename std::enable_if<std::is_floating_point<T>::value, double>::type
+}
+
+/** is_arithmetic */
+template<typename T> typename std::enable_if<std::is_floating_point<T>::value, double>::type
 	normalizeArg(const T& arg) {
 		cout << "--is_arithmetic" << endl;
 		return arg;
-	}
+}
 
-	const char* normalizeArg(const string& arg)
-	{
-		return arg.c_str();
-	}
+const char* normalizeArg(const string& arg)
+{
+	return arg.c_str();
+}
 
-	/** NOT string => fail /
-	template<typename T>
-	std::string helper(const T& arg) {
-		cout << "FAIL" << endl;
-		throw invalid_argument("Translator::getString() does not accept argument type \"" + typeid(T).name() + "\".");
-	}*/
+/** NOT string => fail /
+template<typename T>
+std::string helper(const T& arg) {
+cout << "FAIL" << endl;
+throw invalid_argument("Translator::getString() does not accept argument type \"" + typeid(T).name() + "\".");
+}*/
 
-	template<typename... Ts> void safe_print(const char* str, const Ts&... ts)
-	{
-		cout << printf(str, normalizeArg(ts)...) << endl;
-	}
+template<typename... Ts> void safe_print(const char* str, const Ts&... ts)
+{
+	cout << printf(str, normalizeArg(ts)...) << endl;
+}
