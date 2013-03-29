7a659a0a58900720ba9950d8e3e5f71b7b130724
Cyril Leroux <cyril.leroux@cgi.com>
2013-03-29 16:34:26 +0100

    Updated 19_socket

--- 19_socket/main.cpp
+++ 19_socket/main.cpp
@@ -5,9 +5,13 @@
 #include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
-int boost_async_timer();
-int boost_run_server(const unsigned short& port);
+void display_header();
+void display_menu();
+int get_user_input();
 
+int boost_async_timer();
+int boost_run_client();
+int boost_run_server(const unsigned short& port);
 std::string hostname();
 std::string ip_address();
 int receive(const unsigned short& port);
@@ -16,41 +20,24 @@ int main()
 {
 	bool exitLoop = false;
 
-	std::cout << "/**********************************/" << std::endl;
-	std::cout << "/*** Welcome to Socket Project. ***/" << std::endl;
-	std::cout << "/**********************************/" << std::endl;
-	std::cout << std::endl;
+	display_header();
 
 	while (!exitLoop) {
-		std::cout << "Please, choose an item in the menu :" << std::endl;
-		std::cout << "  1. Async Timer"	<< std::endl;
-		std::cout << "  2. Hostname"	<< std::endl;
-		std::cout << "  3. Ip Adress"	<< std::endl;
-		std::cout << "  4. Client"		<< std::endl;
-		std::cout << "  5. Server"		<< std::endl;
-		std::cout << "  6. Exit"		<< std::endl;
-		std::cout << std::endl;
 
-		// Wait for client to make a choice.
-		std::cout << "Enter a number : ";
-		std::string userInput;
-		std::cin >> userInput;
-		std::cout << std::endl;
+		display_menu();
 
-		// Transform userInput into exploitable value
-		int menuItem = 0;
-		try { 
-			menuItem = std::stoi(userInput);
+		
+		int menuItem;
+		try {
+			menuItem = get_user_input();
 		} catch (const std::exception&) {
-			std::cout << "'" <<userInput << "' is not a number." << std::endl;
-			std::cout << std::endl;
 			continue;
 		}
 
 		switch (menuItem) {
 
 		case 1 : // Async Timer
-			std::cout << "Launching 'Async Timer'." << std::endl;
+			std::cout << "Launching 'Async Timer'..." << std::endl;
 			boost_async_timer();
 			break;
 
@@ -59,7 +46,7 @@ int main()
 			break;
 
 		case 3 : // Ip Adress
-			std::cout << "Your ip adress is '" << ip_address() << "'" << std::endl;
+			std::cout << "Your ip address is '" << ip_address() << "'" << std::endl;
 			break;
 
 		case 4 : // Client
@@ -102,6 +89,50 @@ int main()
 }
 
 /* ***************************************************** */
+/* ***              GUI : menu, etc.                 *** */
+/* ***************************************************** */
+void display_header()
+{
+	std::cout << "/**********************************/" << std::endl;
+	std::cout << "/*** Welcome to Socket Project. ***/" << std::endl;
+	std::cout << "/**********************************/" << std::endl;
+	std::cout << std::endl;
+}
+
+void display_menu()
+{
+	std::cout << "Please, choose an item in the menu :" << std::endl;
+	std::cout << "  1. Async Timer"	<< std::endl;
+	std::cout << "  2. Hostname"	<< std::endl;
+	std::cout << "  3. Ip Adress"	<< std::endl;
+	std::cout << "  4. Client"		<< std::endl;
+	std::cout << "  5. Server"		<< std::endl;
+	std::cout << "  6. Exit"		<< std::endl;
+	std::cout << std::endl;
+}
+
+int get_user_input()
+{
+	// Wait for client to make a choice.
+	std::cout << "Enter a number : ";
+	std::string userInput;
+	std::cin >> userInput;
+	std::cout << std::endl;
+
+	// Transform userInput into exploitable value
+	int menuItem = 0;
+	try { 
+		menuItem = std::stoi(userInput);
+	} catch (const std::exception&) {
+		std::cout << "Bad argument : '" << userInput << "' is not a number." << std::endl;
+		std::cout << std::endl;
+		throw std::exception();
+	}
+
+	return menuItem;
+}
+
+/* ***************************************************** */
 /* ***              Boost Async Timer                *** */
 /* ***************************************************** */
 void print(const boost::system::error_code& /*e*/, boost::asio::deadline_timer* timer, int* count)
@@ -136,9 +167,26 @@ int boost_async_timer()
 	return 0;
 }
 
+
+/* ***************************************************** */
+/* ***                 Boost Client                  *** */
+/* ***************************************************** */
+class tcp_client {
+	// TODO: write tcp_client class
+};
+
+int boost_run_client()
+{
+	return 0;
+}
+
 /* ***************************************************** */
 /* ***                 Boost Server                  *** */
 /* ***************************************************** */
+class tcp_server {
+	// TODO: write tcp_server class
+};
+
 std::string make_daytime_string()
 {
 	std::time_t now = std::time(0);
