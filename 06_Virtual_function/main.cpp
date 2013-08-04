b3c9b3729c29986ef3759654ceef5f076b06e831
Cyril Leroux <cyril0leroux@gmail.com>
2013-08-04 12:18:38 +0200

    Updated virtual functions experiments

--- 06_Virtual_function/main.cpp
+++ 06_Virtual_function/main.cpp
@@ -5,11 +5,18 @@ public:
 	Shape() {
 		std::cout << "Building Shape" << std::endl;
 	}
-
 	void draw() {
 		std::cout << "Shape::draw" << std::endl;
+		do_draw();
 	}
 	virtual void virtual_draw() = 0;
+
+private:
+	Shape& operator=(const Shape&);
+
+	virtual void do_draw() {
+		std::cout << "Shape::do_draw" << std::endl;
+	}
 };
 
 class Square : public Shape {
@@ -18,10 +25,18 @@ public:
 		std::cout << "Building Square" << std::endl;
 	}
 
-	void draw() {
-		std::cout << "Square::draw" << std::endl;
+	virtual void virtual_draw() override {
+		std::cout << "Square::virtual_draw" << std::endl;
 	}
-	virtual void virtual_draw() override { draw(); }
+
+	Square get() {
+		return Square(*this);
+	}
+
+private:
+	virtual void do_draw() {
+		std::cout << "Square::do_draw" << std::endl;
+	}
 };
 
 
@@ -31,10 +46,14 @@ public:
 		std::cout << "Building Triangle" << std::endl;
 	}
 
-	void draw() {
-		std::cout << "Triangle::draw" << std::endl;
+	virtual void virtual_draw() override { 
+		std::cout << "Triangle::virtual_draw" << std::endl;
 	}
-	virtual void virtual_draw() override { draw(); }
+
+private:
+	virtual void do_draw() {
+		std::cout << "Triangle::do_draw" << std::endl;
+	}
 };
 
 class WeirdShape : public Square, public Triangle {
@@ -43,23 +62,28 @@ public:
 		std::cout << "Building WeirdShape" << std::endl;
 	}
 
-	void draw() {
-		std::cout << "WeirdShape::draw" << std::endl;
+	virtual void virtual_draw() override {
+		std::cout << "WeirdShape::virtual_draw" << std::endl;
 	}
-	virtual void virtual_draw() override { draw(); }
+
+private:
+	virtual void do_draw() {
+		std::cout << "WeirdShape::do_draw" << std::endl;
+	}
 };
 
 int main() {
 	Square square;
+	square.draw();
 	square.virtual_draw();
 	std::cout << std::endl;
 
 	Shape* triangle = new Triangle();
+	triangle->draw();
 	triangle->virtual_draw();
 	std::cout << std::endl;
 
 	WeirdShape weird;
-	weird.draw();
 	weird.virtual_draw();
 
 
