b3e58ed8eda5dc5d604cd758b3d7d492d1564869
Cyril Leroux <cyril.leroux@cgi.com>
2013-08-07 08:15:08 +0200

    Playing with uninitialized variables

--- 01_FileIO/01_FileIO.cpp
+++ 01_FileIO/01_FileIO.cpp
@@ -1,7 +1,27 @@
 // 01_FileIO.cpp : Defines the entry point for the console application.
+#include <iostream>
 
+int uninitialized_global;
+
+// !!! Compile time failure : const object must be initialized if not extern
+//const int uninitialized_global_const;
 
 int main()
 {
+	int uninitialized_local;
+	// !!! Compile time failure : uninitialized local variable 'uninitialized_local' used
+	//std::cout << uninitialized_local << std::endl;
+	
+	// Undefined behavior but prints a value
+	std::cout << uninitialized_global << std::endl;
+
+	// Program does not even reach this point if const not initialized
+	//std::cout << uninitialized_global_const << std::endl;
+
+	int y = 10;
+	std::cout << y << std::endl;
+
+	system("PAUSE");
+
 	return 0;
 }
